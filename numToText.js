8507e7410e83493e1b5de6e0c8e239fe825ff0c7
Keith Wong <keith.wong3672@gmail.com>
2016-03-18 13:45:14 -0700

    Create code to recursively replace numbers in string with text

--- numToText.js
+++ numToText.js
@@ -14,5 +14,32 @@ Ex: numToText("I have 5 dogs and 6 ponies"); // returns "I have five dogs and si
 */
 
 var numToText = function(str) {
+  // Create lookup table for numbers 0 - 9
+  var numTable = {
+    0 : "zero",
+    1 : "one",
+    2 : "two",
+    3 : "three",
+    4 : "four",
+    5 : "five",
+    6 : "six",
+    7 : "seven",
+    8 : "eight",
+    9 : "nine"
+  };
+  // Create variable and assign with first character in string
+  var firstChar = str.slice(0, 1);
 
+  // Create stop condition - looped through entire string
+  if (str.length === 0) {return ''}
+  // If in first character in lookup table
+  if (str.hasOwnProperty(firstChar)) {
+    // Change value of firstChar to value in lookup table
+    firstChar = numTable[firstChar];
+  }
+  // return first character and recursively call with new string (string - 1st character of string)
+  return firstChar + numToText(str.slice(1));  
 };
+
+console.log(numToText("0 I have 5 dogs and 6 ponies"));
+
